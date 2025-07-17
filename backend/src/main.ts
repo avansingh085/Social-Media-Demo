239a264334801778836a276851b3d03dd9ecf799
Avan <avansingh085@gmail.com>
2025-07-17 06:29:29 +0530

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000','https://social-media-demo-9cyf.onrender.com'],
+    origin: ['http://localhost:3000','https://social-media-demo-three.vercel.app'],
     credentials: true,
   });
   await app.listen(process.env.PORT || 4000);
